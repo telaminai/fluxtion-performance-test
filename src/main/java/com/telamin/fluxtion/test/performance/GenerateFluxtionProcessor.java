3ceaabd995be725084dff0a1969e3d0504ed2cfe
greg higgins <greg.higgins@v12technology.com>
2026-03-15 10:18:26 +0000

    remove `Main.java`, update encoding configuration, switch to Corretto JDK 21, and add Maven wrapper

--- src/main/java/com/telamin/fluxtion/test/performance/GenerateFluxtionProcessor.java
+++ src/main/java/com/telamin/fluxtion/test/performance/GenerateFluxtionProcessor.java
@@ -3,11 +3,8 @@ package com.telamin.fluxtion.test.performance;
 import com.telamin.fluxtion.builder.extern.spring.FluxtionSpring;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import static com.telamin.fluxtion.builder.compile.generation.EventProcessorGenerator.SOURCE_GENERATOR_ID_PROPERTY;
-
 public class GenerateFluxtionProcessor {
     public static void main(String[] args) {
-        System.setProperty(SOURCE_GENERATOR_ID_PROPERTY, "local");
         FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext(
                 "src/main/resources/benchmark-graph.xml"
         );
